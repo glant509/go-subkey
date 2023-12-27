72dffac2e95f2ee748b3a70c2d1f1d920a6522c5
glant <glant@hirect.in>
2023-12-27 11:29:43 +0800

    improve: change slice len scheme to uint64 in sacle.go.427

--- scale/scale.go
+++ scale/scale.go
@@ -32,7 +32,7 @@ import (
 // has to rely on Go's reflection and thus is notably slower.
 // Feature parity is almost full, apart from the lack of support for u128 (which are missing in Go).
 
-const maxUint = ^uint(0)
+const maxUint = ^uint64(0)
 const maxInt = int(maxUint >> 1)
 
 // Encoder is a wrapper around a Writer that allows encoding data items to a stream.
@@ -427,9 +427,9 @@ func (pd Decoder) DecodeIntoReflectValue(target reflect.Value) error {
 		if codedLen64.Uint64() > math.MaxUint64 {
 			return errors.New("encoded array length is higher than allowed by the protocol (32-bit unsigned integer)")
 		}
-		//if codedLen64.Uint64() > uint64(maxInt) {
-		//	return errors.New("encoded array length is higher than allowed by the platform")
-		//}
+		if codedLen64.Uint64() > uint64(maxInt) {
+			return errors.New("encoded array length is higher than allowed by the platform")
+		}
 		codedLen := int(codedLen64.Uint64())
 		targetLen := target.Len()
 		if codedLen != targetLen {
